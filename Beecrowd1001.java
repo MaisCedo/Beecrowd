bdf6fec229646bb676410abca658bfe0aab4edea
MaisCedo <Gabriel_Macedo83@hotmail.com>
2022-09-12 18:29:04 -0300

    primeiro desafio do beecrowd

---
+++ Beecrowd1001.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Beecrowd1001 {
+    public static void main(String[] args) throws Exception {
+       Scanner ler = new Scanner(System.in);
+       int A,B,X;
+
+       System.out.println("Entre com o primeiro inteiro:");
+       A = ler.nextInt();
+
+       
+       System.out.println("Entre com o segundo inteiro:");
+       B = ler.nextInt();
+
+       X = A+B;
+       System.out.println("X = " + X);
+       
+    }
+}
